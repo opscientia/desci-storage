5633a42eaf030f8967bcb2ad50c098c78847dc47
Kinshuk Kashyap <kinshukkashyap.me@gmail.com>
2022-05-17 20:06:22 +0530

    Status skeleton for task queue

--- storage_estuary/datalad-dashboard/src/App.js
+++ storage_estuary/datalad-dashboard/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Link,
     Outlet
@@ -7,21 +7,37 @@ import {
 import Home from './routes/Home'
 import About from './routes/About'
 
+import Status from './components/Status'
+
 function App() {
-  return (
+
+    const [status, setStatus] = useState(null)
+
+    useEffect(() => {
+        fetch("http://localhost:3001/status")
+        .then(res => res.json())
+        .then(data => {
+            setStatus(data.status)
+            console.log("response data for /status ", data)
+        })
+    }, [])
+    // empty square brackets means no dependencies for this var, loads only once
+
+    return (
       <div style={{padding: "1rem"}}>
         <h1> datalad dashboard </h1>
 
         <nav style={{borderBottom: "solid 3px", paddingBottom: "1rem",}} >
             <Link to="/home">Home</Link> |{" "}
-            <Link to="/about">About</Link>
+            <Link to="/about">About</Link> |{" "}
+            <Status status={status}/>
         </nav>
 
         <div style={{paddingTop: "1rem", paddingBottom: "1rem",}}>
             <Outlet />
         </div>
       </div>
-  );
+    );
 }
 
 export default App;
---
+++ storage_estuary/datalad-dashboard/src/components/Status.js
@@ -0,0 +1,5 @@
+function Status (props) {
+    return <> STATUS: {props.status} </>
+}
+
+export default Status;
