e75a654c900fd9eef18b0c5e0d2afa3a51f7ffde
Kinshuk Kashyap <kinshukkashyap.me@gmail.com>
2022-06-19 16:06:07 +0530

    UI additions

--- storage_estuary/datalad-dashboard/src/routes/Dataset.js
+++ storage_estuary/datalad-dashboard/src/routes/Dataset.js
@@ -1,24 +1,41 @@
 import { useParams } from "react-router-dom";
-import { getDataset } from '../utils/data'
+import { useState, useEffect } from "react"
 
 export default function Dataset() {
     let params = useParams()
-    // TODO: Create POST endpoint on mongo server for individual datasets
-    let dataset = getDataset(params.datasetName)
-    // useEffect(() => {
-    //     fetch()
-    // }, [])
+
+    const [dataset, setDataset] = useState(null)
+    // const [name, setName] = useState('')
+
+    const PUT_URL = "http://localhost:3001/dataset"
+    useEffect(() => {
+        fetch(PUT_URL, {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"name":params.datasetName})
+        })
+        .then(res => res.json())
+        .then(result => {
+            setDataset(result.dataset)
+        })
+    }, [])
 
     return (
         <>
             <>
                 {dataset &&
                     <main style={{ padding: "1rem" }}>
-                        <h2>{dataset.name}</h2>
-                        <p>{dataset.description}</p>
-                        <p>Archived: {dataset.added ? 'yes' : 'no'}</p>
-                        <p>CID: {dataset.CID}</p>
-                        <p>Size: {dataset.size}</p>
+                        <h2>{dataset.dataset_name_datalad}</h2>
+
+                        <p>Archived: <em>{dataset.archived ? 'yes' : 'no'}</em></p>
+
+                        {dataset.archived &&
+                            <>
+                                <p>Description: {dataset.description}</p>
+                                <p>CID: {dataset.CID}</p>
+                                <p>Size: {dataset.size}</p>
+                            </>
+                        }
                     </main>
                 }
             </>
