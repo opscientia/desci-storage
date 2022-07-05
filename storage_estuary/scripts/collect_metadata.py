c5247d0497bbb6ca0341e35c3655e4dc8cae5639
Kinshuk Kashyap <kinshuk.other@gmail.com>
2022-07-05 07:57:58 +0000

    extracting descriptions from datapackage.json and README.md

--- storage_estuary/scripts/collect_metadata.py
+++ storage_estuary/scripts/collect_metadata.py
@@ -10,6 +10,8 @@ if len(sys.argv) < 4:
 
 DATASET_PATH, JSON_OUTPUT_DIR, DATASET_NAME = sys.argv[1:]
 
+print(f"DATASET_PATH: {DATASET_PATH}\nJSON_OUTPUT_DIR: {JSON_OUTPUT_DIR}\nDATASET_NAME: {DATASET_NAME}")
+
 output = subprocess.Popen(f"cd {DATASET_PATH} && find . -xtype l", stdout=subprocess.PIPE, shell=True)
 
 output_lines = [line_bytes.decode('utf-8') for line_bytes in output.communicate()[0].split()]
@@ -17,15 +19,37 @@ output_lines = [line_bytes.decode('utf-8') for line_bytes in output.communicate(
 for line in output_lines:
     print(line)
 
+# description will be a map - source -> data
+# for ex: "datapackage.json" : <info from that file>
+#         "README.md": <string copied over from that file>
+# ... etc.
+
+description_map = {}
+
+try:
+    print(f"Trying to extract datapackage.json {DATASET_NAME}/datapackage.json...")
+    with open(f"{DATASET_PATH}/datapackage.json", mode="r") as f:
+        description_map["datapackage.json"] = json.load(f)
+except:
+    print("couldn't open datapackage.json, skipping")
+
+try:
+    print(f"Trying to open README.md {DATASET_NAME}/README.md")
+    with open(f"{DATASET_NAME}/datapackage.json", mode="r") as f:
+        description_map["README.md"] = f.read()
+except:
+    print("couldn't open README.md in root dir, skipping")
+
 metadata = {
             "name": DATASET_NAME,
             "dataset_size": None,
-            "description": None,
+            "description": description_map,
             "estuary_dealid": None,
             "CID": None,
             "missing_files": output_lines, # format for this? for now a list of file paths rel to root
             "num_pieces": 1, # default for <32GB
             # "piece_CIDs": [array of CIDs in order]
+
 }
 
 metadata_json_object = json.dumps(metadata)
